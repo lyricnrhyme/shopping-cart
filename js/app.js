a98bdc65fcf4ad019b09e03e1bc2afe44aa657d2
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-02-04 19:11:05 -1000

    Added icons using DOM

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@
 var shoppingCart = [{listImg: "../images/happy.png", itemName: "Happy Friend", itemDescription: "Someone who will always remind you to smile!", price: 100.00},
 	{listImg: "../images/Github.png", itemName: "Github", itemDescription: "The world's leading software developing platform", price: 1000.00},
 	{listImg: "../images/SquareUp.png", itemName: "Square", itemDescription: "It's Easy to Take Credit Cards", price: 1000.00},
-	{listImg: "../images/LetterH.png", itemName: "Letter H", itemDescription: 'Start your words right like "Hello", "Hi, "Happy", "Horchata", etc', price: 1000.00},
+	{listImg: "../images/LetterH.jpg", itemName: "Letter H", itemDescription: 'Start your words right like "Hello", "Hi, "Happy", "Horchata", etc', price: 1000.00},
 	{listImg: "../images/Tinder.png", itemName: "Tinder", itemDescription: "Swipe. Match. Chat.", price: 1000.00}];
 
 function getData(data){
-	for(var i=0; i<data.length; i++) {
+	for(var i=0; i<shoppingCart.length; i++) {
 		// var divElem = makeElements("div", "itemsBox", "test");
 		// main.appendChild(divElem);
 		
@@ -18,26 +18,40 @@ function getData(data){
 
 		// var priceBox = makeElements("div", "itemPrice", data[i].price);
 
+		var wholeCart = document.getElementById("wholeCart");
 
-		var divElem = document.createElement("div");
-		divElem.className = "itemsBox";
-		main.appendChild(divElem);
+		var cartElem = document.createElement("div");
+		cartElem.className = "cartItem";
+		wholeCart.appendChild(cartElem);
 
-		var productBox = document.createElement("div");
-		productBox.className = "itemNames";
-		productBox.innerHTML = data[i].product;
-		divElem.appendChild(productBox);
+		var imgElem = document.createElement("IMG");
+		imgElem.className = "listImg";
+		imgElem.setAttribute("src", data[i].listImg);
+		cartElem.appendChild(imgElem);
+
+		console.log("test");
+
+	// 	var divElem = document.createElement("div");
+	// 	divElem.className = "itemsBox";
+	// 	main.appendChild(divElem);
+
+	// 	var productBox = document.createElement("div");
+	// 	productBox.className = "itemNames";
+	// 	productBox.innerHTML = data[i].product;
+	// 	divElem.appendChild(productBox);
 	
-	var priceBox = document.createElement("div");
-		priceBox.className = "itemPrice";
-		priceBox.innerHTML = data[i].price;
-		divElem.appendChild(priceBox);
-	var descripBox = document.createElement("div");
-		descripBox.className = "itemPrice";
-		descripBox.innerHTML = data[i].descrip;
-		divElem.appendChild(descripBox);
+	// var priceBox = document.createElement("div");
+	// 	priceBox.className = "itemPrice";
+	// 	priceBox.innerHTML = data[i].price;
+	// 	divElem.appendChild(priceBox);
+	// var descripBox = document.createElement("div");
+	// 	descripBox.className = "itemPrice";
+	// 	descripBox.innerHTML = data[i].descrip;
+	// 	divElem.appendChild(descripBox);
 	}
 }
+
+getData(shoppingCart);
 
 // function makeElements(elem, marker, data){
 // 	var createBox = document.createElement(elem);

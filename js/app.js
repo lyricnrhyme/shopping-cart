8ac9418110d51168bc0cb2b40460fd8eaabf4e61
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-02-04 19:27:18 -1000

    Added in Cart Item Name, Item Description, Item Price, pre-fixing Price position

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 var shoppingCart = [{listImg: "../images/happy.png", itemName: "Happy Friend", itemDescription: "Someone who will always remind you to smile!", price: 100.00},
 	{listImg: "../images/Github.png", itemName: "Github", itemDescription: "The world's leading software developing platform", price: 1000.00},
 	{listImg: "../images/SquareUp.png", itemName: "Square", itemDescription: "It's Easy to Take Credit Cards", price: 1000.00},
-	{listImg: "../images/LetterH.jpg", itemName: "Letter H", itemDescription: 'Start your words right like "Hello", "Hi, "Happy", "Horchata", etc', price: 1000.00},
+	{listImg: "../images/LetterH.jpg", itemName: "Letter H", itemDescription: 'Start your words right like "Hello", "Hi, "Happy", "Horchata", and more!', price: 1000.00},
 	{listImg: "../images/Tinder.png", itemName: "Tinder", itemDescription: "Swipe. Match. Chat.", price: 1000.00}];
 
 function getData(data){
@@ -29,8 +29,24 @@ function getData(data){
 		imgElem.setAttribute("src", data[i].listImg);
 		cartElem.appendChild(imgElem);
 
-		console.log("test");
+		var infoElem = document.createElement("div");
+		infoElem.className = "itemInfo";
+		cartElem.appendChild(infoElem);
 
+		var nameElem = document.createElement("div");
+		nameElem.className = "itemName";
+		nameElem.innerHTML = data[i].itemName;
+		infoElem.appendChild(nameElem);
+
+		var detailElem = document.createElement("div");
+		detailElem.className = "itemDescription";
+		detailElem.innerHTML = data[i].itemDescription;
+		infoElem.appendChild(detailElem);
+
+		var priceElem = document.createElement("div");
+		priceElem.className = "price";
+		priceElem.innerHTML = data[i].price;
+		cartElem.appendChild(priceElem);
 	// 	var divElem = document.createElement("div");
 	// 	divElem.className = "itemsBox";
 	// 	main.appendChild(divElem);
